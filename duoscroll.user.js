353bb8154b7a048a0052b26ec61bb21f5278e931
friendly-trenchcoat <tippywigwom@yahoo.com>
2016-06-22 13:50:40 -0400

    Update duoscroll.user.js

--- duoscroll.user.js
+++ duoscroll.user.js
@@ -6,9 +6,11 @@
 // @require	       http://code.jquery.com/jquery-latest.min.js
 // ==/UserScript==
 
-// NOTE: eq(0) means, use the first non-gold icon. If you want to go to the second, use eq(1); the third, eq(2); etc.
-var element = $("span[class*='unlocked']").eq(0).parent().parent().parent().prev(); 
-
-$('html, body').animate({
-    scrollTop: $(element).offset().top
+setInterval(function() { // this is becuase duo doesnt reload the page when going between tabs
+    // NOTE: eq(0) means, use the first non-gold icon. If you want to go to the second, use eq(1); the third, eq(2); etc.
+    var element = $("span[class*='unlocked']").eq(0).parent().parent().parent().prev(); 
+    
+    $('html, body').animate({
+        scrollTop: $(element).offset().top
+    }, 2000);
 }, 2000);
